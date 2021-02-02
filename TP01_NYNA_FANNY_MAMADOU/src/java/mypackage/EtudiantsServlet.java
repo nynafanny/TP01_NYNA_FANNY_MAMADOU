289a8828ff3bcbcd70e9f3e603b068a287f448ff
Fanny94-star <nynamamadu@gmail.com>
2021-02-02 17:41:08 +0000

    first commit

--- TP01_NYNA_FANNY_MAMADOU/src/java/mypackage/EtudiantsServlet.java
+++ TP01_NYNA_FANNY_MAMADOU/src/java/mypackage/EtudiantsServlet.java
@@ -7,7 +7,6 @@ package mypackage;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author NYNA FANNY
  */
-public class ExperimentServlet extends HttpServlet {
+public class EtudiantsServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -29,38 +28,26 @@ public class ExperimentServlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            
             throws ServletException, IOException {
-        InetAddress IP = InetAddress.getLocalHost();
-        String myIP = IP.getHostAddress();
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ExperimentServlet</title>");            
+            out.println("<title>Servlet EtudiantsServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Ma Premiere Servlet</h1>");
-            out.print("<h2>Information concernant l'adresse IP et le navigateur utilisé par le client :</h2>");
-            out.print("<p> Adresse IP du client : "+myIP +"</p>");
-            out.print("<p> Navigateur du client :"+request.getHeader("user-agent")+" </p>");
-            
-            out.print("<h2>Information concernant la requête du client :</h2>");
-            out.print("<p> Methode : "+request.getMethod()+"</p>");
-            out.print("<p> Protocole : "+request.getProtocol()+"</p>");
-            out.print("<p> URI démandé : "+request.getRequestURI()+"</p>");
-            
-            out.print("<h1> Information concernant l'entête de la requête : </h1>");
-            out.print("<P>Host:  "+request.getServerName()+" : "+request.getServerPort()+"</p>");
-            out.print("<p> User-agent:"+request.getCharacterEncoding()+"</p>");
-            out.print("<p> Accept: "+request.getHeader("Accept")+"</p>");
-            out.print("<p> Accept-language: "+request.getLocale()+"</p>");
-            out.print("<p> Accept-encoding: "+request.getHeader("Accept-encoding")+"</p>");
-            out.print("<p> Accept-charset:"+request.getHeader("Accept-charset")+" </p>");
-            out.print("<p> kip-alive: "+request.getHeader("kip-alive")+"</p>");
-            out.print("<p>connexion: "+request.getHeader("connexion")+"</p>");
+            out.println("<h1>Formulaire étudiant </h1>");
+            out.println(" <form action=action_page.php>");
+            out.println("    <label for=fname>Nom :</label><br>");
+            out.println("    <input type=text id=fname name=fname><br><br>");
+            out.println("    <label for=lname>Prenom :</label><br>");
+            out.println("    <input type=text id=lname name=lname ><br><br>");
+            out.println("    <label for=lname>Email :</label><br>");
+            out.println("    <input type=text id=lname name=email ><br><br>");
+            out.println("    <input type=\"submit\" value=\"Sauvegarder\">");
+            out.println("</form>");
             out.println("</body>");
             out.println("</html>");
         }
